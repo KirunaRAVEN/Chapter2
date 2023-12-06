eb150226ae97b2e40d5d130a24139a955efaa71d
Louis <louis.barboutie@gmail.com>
2023-12-06 16:31:10 +0100

    Add header comment

--- InterfaceSoftware/rollingAverage.py
+++ InterfaceSoftware/rollingAverage.py
@@ -1,2 +1,13 @@
-def rollingAverage():
-    return 0
+'''
+    AUTHOR: Louis-Hendrik Barboutie
+    CONTACT: louis.barboutie@gmail.com, loubar-3@student.ltu.se
+
+    SUMMARY: This function computes the moving average over the points fed to it in the dataList, intended to be the data displayed on screen
+'''    
+def rollingAverage(dataList):
+    rollingAverage = 0
+    for i in range(len(dataList)):
+        rollingAverage += dataList[i]
+    rollingAverage /= len(dataList)
+
+    return rollingAverage
