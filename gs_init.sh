3b7b6984752b29c941fee1c17be13c7937bc11b9
LouisB <79802588+TakashiSenpai@users.noreply.github.com>
2024-03-12 15:25:09 +0100

    add auto login to rock

--- gs_init.sh
+++ gs_init.sh
@@ -7,12 +7,14 @@
 # navigate to working directory
 cd Documents/ROCK_SOFTWARE/
 
-# ping the rock for 5s to check packet loss
-
-ping -c 5 rock-4c-plus.local | grep 'received'
+# ping the rock to check packet loss
+ping -c 1 rock-4c-plus.local | grep 'received'
 
 # establish ssh connection 
-ssh rock@rock-4c-plus.local 'cd scripts/ ; python3 serial_reader.py'
+sshpass -p "rock" ssh -o StrictHostKeyChecking=no -t rock@rock-4c-plus.local "echo 'rock' | sudo -S python3 scripts/serial_reader.py" &  
 
-tilix -a session-add-right -x "python3 generateDummyData.py" 
-tilix -a session-add-down -x "python3 live_grapher_V3.py"
+#echo pass | ssh -tt rock@rock-4c-plus.local "python3 serial_reader.py"
+#ssh rock@rock-4c-plus.local 
+
+tilix -a session-add-right -x "sleep 1; sshpass -p 'rock' ssh rock@rock-4c-plus.local 'tail -f scripts/data.csv' | tee data.csv" 
+#tilix -a session-add-down -x "python3 live_grapher_V3.py"
