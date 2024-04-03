595771753fea063f8ca037306ad0a6585f708390
LouisB <79802588+TakashiSenpai@users.noreply.github.com>
2024-04-03 09:18:05 +0200

    Add comments

--- gs_init.sh
+++ gs_init.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-# execute bash script with 'source gs_init.sh'
+# execute this bash script in a terminal with the command 'source gs_init.sh'
+# alternatively, edit the .bashrc file in the home/ directory by adding the line ''
+# the script can then be launched by the shortcut 'gsi'
 
 # navigate to working directory
 cd ~/Documents/ROCK_SOFTWARE/
+
+# ping the rock three times to check packet loss
 echo 'Pinging rock ...'
+ping -c 3 rock-4c-plus.local | grep 'received'
 
-# ping the rock to check packet loss
-ping -c 1 rock-4c-plus.local | grep 'received'
-
-# establish ssh connection 
+# establish ssh connection and launch the serial reader code on the rock 
 sshpass -p "rock" ssh rock@rock-4c-plus.local "cd scripts; echo 'rock' | sudo -S python3 serial_reader.py" &
 
+# open new terminal and stream the data to the laptop
 gnome-terminal -- sh -c "sshpass -p 'rock' ssh rock@rock-4c-plus.local 'tail -f scripts/data.csv' | tee data.csv"  
 
+# open new terminal and run the interface software
 gnome-terminal -- sh -c "python3 live_grapher_V3.py"
