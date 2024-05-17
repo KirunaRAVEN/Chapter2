6155727a2160f3e20918f017201a09e83e34231d
eemmyk <eemmyk-8@student.ltu.se>
2024-05-17 08:18:34 +0200

    Were going to the moon with this one

--- TestStandSoftware/Sensors.cpp
+++ TestStandSoftware/Sensors.cpp
@@ -8,12 +8,26 @@
  */
 
 #include "Globals.h"
+#include <Arduino.h>
 //#include "LatestValues.h"
 //#include "FaultDetection.h"
 #include "Sensing.h"
 
 void initSensors(){
-  //Nothing to initialize currently
+  // Set the Arduino ADC clock prescaler to get faster analogRead()
+
+    ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
+  
+    // uncomment as required
+    
+    //ADCSRA |= bit (ADPS0);                               //   2  
+    //ADCSRA |= bit (ADPS1);                               //   4  
+    //ADCSRA |= bit (ADPS0) | bit (ADPS1);                 //   8  
+    ADCSRA |= bit (ADPS2);                               //  16 
+    //ADCSRA |= bit (ADPS0) | bit (ADPS2);                 //  32 
+    //ADCSRA |= bit (ADPS1) | bit (ADPS2);                 //  64 
+    //ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // 128
+
 }
 
 void getValuesFromSensors(values_t* values){
--- TestStandSoftware/SerialComms.cpp
+++ TestStandSoftware/SerialComms.cpp
@@ -27,64 +27,11 @@ uint32_t count = 0;
 cppQueue msgBuffer(sizeof(uint16_t), msgBufferSize, FIFO, true);
 
 void initSerial(){
-
   Serial.begin(serialBaud);
-  //Serial.println(serialBaud);
   while (!Serial){}
-  //serialMutex = xSemaphoreCreateMutex();
-  //messageMutex = xSemaphoreCreateMutex();
 
+  //Indicate reset of the system
   Serial.print("\n r\n");
-  
-  //Print the headers for the CSV-file
-  /*
-  Serial.print("Dataline marker");            //Dataline index 1
-  Serial.print(",");
-  Serial.print("Arduino time");               //Arduino time in us. Dataline index 2
-  Serial.print(",");
-  Serial.print("N2 Feeding Pressure");        //N2 Feeding pressure . Dataline index 3
-  Serial.print(",");
-  Serial.print("Line Pressure");               //Line pressure . Dataline index 4 
-  Serial.print(",");
-  Serial.print("Chamber Pressure");           //Chamber pressure . Dataline index 5
-  Serial.print(",");
-  Serial.print("Oxidizer Feeding Pressure");  //Oxidizer Feeding pressure . Dataline index 6
-  Serial.print(",");
-  Serial.print("Load Cell");                  //Load cell for thrust. Dataline index 7
-  Serial.print(",");
-  Serial.print("Bottle Temperature");         //Bottle/Heating blanket temperature. Dataline index 8
-  Serial.print(",");
-  Serial.print("NC");                         //Not connected. Dataline index 9
-  Serial.print(",");
-  Serial.print("Nozzle Temperature");         //Nozzle temperature. Dataline index 10
-  Serial.print(",");
-  Serial.print("Piping temperature");        //Piping temperature. Dataline index 11
-  Serial.print(",");
-  Serial.print("Plume Temperature");          //Plume temperature. Dataline index 12
-  Serial.print(",");
-
-  Serial.print("Dump Button");        //Dump valve button status. Dataline index 13
-  Serial.print(",");
-  Serial.print("Heating Button");     //Heating button status. Dataline index 14
-  Serial.print(",");
-  Serial.print("Ignition Button");    //Ignition button status. Dataline index 15
-  Serial.print(",");
-  Serial.print("N2 Feeding Button");  //N2 Feeding button status. Dataline index 16
-  Serial.print(",");
-  Serial.print("Oxidizer Button");    //Oxidizer valve button status. Dataline index 17
-  Serial.print(",");
-
-  Serial.print("Ignition Relay");     //Ignition SW state. Dataline index 18
-  Serial.print(",");
-  Serial.print("Oxidizer Valve");     //Valve SW state. Dataline index 19
-  Serial.print(",");
-  Serial.print("SW Mode");            //Current SW mode. Dataline index 20
-  Serial.print(",");
-  Serial.print("SW Substate");        //Current SW substate. Dataline index 21
-  Serial.print(",");
-  Serial.print("Message");            //Message field. Dataline index 22
-  Serial.print("\n");
-  */
 }
 
 void writeValues(values_t values, statusValues_t statusValues){
@@ -93,6 +40,8 @@ void writeValues(values_t values, statusValues_t statusValues){
   if (!msgBuffer.isEmpty()){
     msgBuffer.pop(&msgIndex);
   }
+
+  uint32_t sentTimeValue = (uint32_t) (values.timestamp >> 3); // Bitshift by 3 to get 8*72 minutes of runtime without 32bit overflow
 
   // First 32bit data - sent always
   long combinedValue1 = values.N2FeedingPressure;
@@ -112,18 +61,6 @@ void writeValues(values_t values, statusValues_t statusValues){
   combinedValue2 = combinedValue2 << (3) | statusValues.mode;
   combinedValue2 = combinedValue2 << (3) | statusValues.subState;
 
-  //Third 32bit data - sent at most every 100ms
-  long combinedValue3 = values.nozzleTemperature;
-  combinedValue3 = combinedValue3 << (14) | values.pipingTemperature;
-  combinedValue3 = combinedValue3 << (3) | msgIndex & 7; //first part of the message bits
-
-  //Fourth 32bit data - sent at most every 100ms
-  long combinedValue4 = values.bottleTemperature;
-  combinedValue4 = combinedValue4 << (3) | (msgIndex >> 3) & 7; //second part of the message bits
-  combinedValue4 = combinedValue4 << (10) | values.IR; //For unkown reasons this didn't work with having IR as the first value
-  
-  uint32_t sentTimeValue = (uint32_t) (values.timestamp >> 3);
-
   Serial.print(sentTimeValue);     //Arduino time in us. Dataline index 2
   Serial.print(",");
   Serial.print(combinedValue1);
@@ -131,6 +68,17 @@ void writeValues(values_t values, statusValues_t statusValues){
   Serial.print(combinedValue2);
 
   if (values.slowUpdated == true){
+
+    //Third 32bit data - sent at most every 100ms
+      long combinedValue3 = values.nozzleTemperature;
+      combinedValue3 = combinedValue3 << (14) | values.pipingTemperature;
+      combinedValue3 = combinedValue3 << (3) | msgIndex & 7; //first part of the message bits
+
+      //Fourth 32bit data - sent at most every 100ms
+      long combinedValue4 = values.bottleTemperature;
+      combinedValue4 = combinedValue4 << (3) | (msgIndex >> 3) & 7; //second part of the message bits
+      combinedValue4 = combinedValue4 << (10) | values.IR; //For unkown reasons this didn't work with having IR as the first value
+      
     Serial.print(",");
     Serial.print(combinedValue3);
     Serial.print(",");
