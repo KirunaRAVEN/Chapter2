6c374f988c78bc5f74b8298320eff7e24b3cee1d
eemmyk <eemmyk-8@student.ltu.se>
2024-05-21 21:23:33 +0200

    csv_patcher update

--- Data_Handling/csv_patcher.m
+++ Data_Handling/csv_patcher.m
@@ -1,14 +1,27 @@
+
+%   CSV patcher
+%   Linearly interpolates the missing timesteps from the data
+%   
+%
+
+copyfile data.csv patched_data.csv
+
 data = readtable("data.csv");
-patchedTable = data;
+patchedTable = readtable("patched_data.csv");
 
 pointCount = height(data);
+
+percentSkip = floor(pointCount / 100);
 
 firstTimeIndex = 0;
 lastTimeIndex = 0;
 
 i = 2;
+
 while i < pointCount
-    
+    if mod(i, percentSkip) == 0
+        fprintf("Patching time skips: <strong>%.1f %%</strong> completed \n", 100 * i / pointCount);
+    end
     if data.ArduinoTime(i-1) == data.ArduinoTime(i) 
         firstTimeIndex = i-1;
         while data.ArduinoTime(i) == data.ArduinoTime(firstTimeIndex)
@@ -26,3 +39,6 @@ while i < pointCount
 end
 
 writetable(patchedTable, "patched_data.csv");
+
+
+
