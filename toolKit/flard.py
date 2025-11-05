b1a927cf8b75b016406e5baca3154c2204561f4c
aurahs <sebhsl-1@student.ltu.se>
2025-11-05 14:56:18 +0100

    routine committing. work is progressing on flard. weird bugs are afoot.

--- toolKit/flard.py
+++ toolKit/flard.py
@@ -1,42 +1,67 @@
-import pyduinocli
+#import pyduinocli
 import git
 import os
+import subprocess
+import sys
+import time
 cli_path = "arduino-cli"
 git_local_path = "./TestBenchRepo"
-repo_link = "https://github.com/MrLuckyOne/gibbgrottan.git"
+repo_link = "https://github.com/KirunaRAVEN/Chapter2.git"
+BRANCH_NAME = "refactor-dev"
 
-def deleteDirectory(pathToDelete):
-    for root, dirs, files in os.walk(pathToDelete):
-        for file in files:
-            file_path = os.path.join(root, file)
-            os.chmod(file_path, 0o777)
-            os.remove(file_path)
-        for directory in dirs:
-            file_path = os.path.join(root, directory)
-            deleteDirectory(file_path)
-            os.chmod(file_path, 0o777)
-            os.rmdir(file_path)
-        return 0
+if __name__ == '__main__':
 
-def getLatestRepo(linkToRepo, endDirectory):
-        deleteDirectory(endDirectory)
-        git.Repo.clone_from(linkToRepo, endDirectory)
-        return 0
-
-def setupArduinoCli(cliPath):
-    try:
-        arduino = pyduinocli.Arduino(cliPath)
-        print(arduino.version())
-        return 0
-    except:
-        print("arduino-cli not found")
-        return 1
+    if "-v" in sys.argv:
+        out = None
+    else:
+        out = subprocess.DEVNULL
 
 
-def main():
-    # Set up the arduino command line interface
-    setupArduinoCli(cli_path)
-    getLatestRepo(repo_link, git_local_path)
+    # set up git backend and check that everything is up to date
+    r = git.Repo(".", search_parent_directories=True)
+    if r.head.ref.name != BRANCH_NAME:
+        print("[!] on invalid branch")
+        # TODO: Change branch
+        exit()
+    else:
+        print("[+] on valid branch")
 
-main()
+    print("[+] pulling the most recent commit")
+    fetchInfos = r.remotes.origin.pull()
+    parsedFetchInfos = [(x.commit.committed_date, x.commit.author.email, x.commit.message) for x in fetchInfos]
 
+    print("most recent pushes:")
+    for i in parsedFetchInfos:
+        print(f"On {time.asctime(time.localtime(i[0]))}, by {i[1]} with message {i[2]}")
+    """
+    localCommit = r.head.object
+    remoteCommit = r.remote().refs[-1].commit #has fixed offset, make it find it dynamically. TODO
+    if localCommit.hexsha != remoteCommit.hexsha:
+        print("[!] on outdated commit")
+        # TODO: pull the most recent commit
+    else:
+        print("[+] everything is up to date")
+
+    """
+
+    # check that burnTime is correct
+    with open("TestStandSoftware/Globals.h") as f:
+        globalConstants = f.readlines()
+
+    for line in globalConstants:
+        if "const int16_t burnTime" in line:
+            line = line.rstrip("\n")
+            print(f"[+] current burn time: {line.split(' ')[-1].rstrip(';')} ms")
+        # TODO: Update BurnTime from here?
+
+
+    # flash the arduino
+    compileCommand = ["arduino-cli", "compile", "--fqbn", "arduino:mbed_portenta:envie_m7", "TestStandSoftware/TestStandSoftware.ino"]
+    flashCommand = ["arduino-cli", "upload", "/dev/ttyACM0", "--fqbn", "arduino:mbed_portenta:envie_m7", "TestStandSoftware/TestStandSoftware.ino"]
+    if subprocess.call(compileCommand, stdout=out, stderr=out):
+        print("[!] compilation failed")
+    else:
+        if subprocess.call(flashCommand, stdout=out, stderr=out):
+            print("[!] flashing failed")
+        else:
+            print("[+] test stand updated")
