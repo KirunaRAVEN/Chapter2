Fixed performance parameters computation

toolKit/Python Analysis New/SCRIPTS/analysis_functions.py
@@ -3,7 +3,7 @@ analysis_functions.py
 
 Author: Francesca Ciacci
 Date: October 6, 2025
-Version: 1.0.0
+Version: 1.0
 Description:
     This script provides functions for analyzing hybrid rocket test data.
 """
@@ -105,7 +105,7 @@ def compute_fuel_flow(data, mfuel_i, m_dot_ox, index_pres, dfuel, port_i, port_m
     return total_fuel, m_dot_fuel
 
 
-def compute_performance(data, column_names, n2o_properties, index_line, index_pres, mass_change, ox_rate, fuel_rate, burn_time):
+def compute_performance(data, column_names, n2o_properties, index_line, index_pres, ox_rate, fuel_rate, burn_time):
     """
     Computes performance parameters:
       - impulse
@@ -123,7 +123,6 @@ def compute_performance(data, column_names, n2o_properties, index_line, index_pr
         n2o_properties (pd.DataFrame): Saturation properties of N₂O loaded from CSV
         index_line (list[int]): Indices of ignition/valve events
         index_pres (int): Start index of steady chamber pressure
-        mass_change (array): Mass differences [ox, fuel, total]
         ox_rate (float): Oxidizer mass flow rate [kg/s]
         fuel_rate (float): Fuel mass flow rate [kg/s]
         burn_time (float): Burn duration [s]
@@ -153,7 +152,7 @@ def compute_performance(data, column_names, n2o_properties, index_line, index_pr
     sigma = np.mean(rolling_std)
 
     # Isp 
-    isp = thrust / (mass_change[2] * 9.81)
+    isp = thrust / ((ox_rate + fuel_rate) * 9.81)
 
     # C* 
     chamber_mean = data.iloc[index_pres:index_line[-1], chamber_col].mean()

toolKit/Python Analysis New/SCRIPTS/helper_functions.py
@@ -3,7 +3,7 @@ helper_functions.py
 
 Author: Francesca Ciacci
 Date: October 6, 2025
-Version: 1.0.0
+Version: 1.0
 Description:
     This script contains utility functions used for data preprocessing, 
     file management, and signal analysis in hybrid rocket test data.
@@ -320,7 +320,7 @@ def mass_flow_rate(data, test_id, prop_mass, column_names, valve_idx=None):
 
     # Δp signal 
     # sqrt_dp = np.sqrt(np.maximum(p_line - p_chamber, 0)) avoid negatives
-    sqrt_dp = np.sqrt(p_line - p_chamber)
+    sqrt_dp = np.sqrt(p_line)
 
     # Time
     time_col = get_column_number(column_names, "ArduinoMegaTime")

toolKit/Python Analysis New/SCRIPTS/main_performance.py
@@ -3,7 +3,7 @@ main_performance.py
 
 Author: Francesca Ciacci
 Date: October 6, 2025
-Version: 1.0.0
+Version: 1.0
 Description:
     Main script for computing hybrid rocket performance parameters.
     It loads test data, computes propellant mass flow rates, 
@@ -75,7 +75,6 @@ port_max = d_param.port_max
 length = d_param.length
 
 total_fuel, m_dot_fuel = analysis_functions.compute_fuel_flow(data,mfuel_i,m_dot_ox,press_index,dfuel,port_i,port_max,length)
-print('Total fuel:', total_fuel[-2])
 
 # Correct burn rate coefficient based on actual fuel consumption
 a = d_param.a
@@ -85,7 +84,7 @@ print('Correct a:', correct_a)
 fuel_rate = np.median(analysis_functions.compute_fuel_flow(data,mfuel_i,m_dot_ox,press_index,dfuel,port_i,port_max,length,correct_a)[1])
 
 # Compute performance
-results = analysis_functions.compute_performance(data,column_names,n2o_properties,line_index,press_index,mass_change,ox_flow,fuel_rate,burn_time)
+results = analysis_functions.compute_performance(data,column_names,n2o_properties,line_index,press_index,ox_flow,fuel_rate,burn_time)
 
 # -------------
 # PRINT RESULTS