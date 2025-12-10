80e56854db65c6fa233215d01adbd53a5f0ccd2a
Diego AW <diegoalmendrow@gmail.com>
2025-12-10 13:00:17 +0100

    Breakout temporary pin assignment and setup

--- controlBox/controlBox.ino
+++ controlBox/controlBox.ino
@@ -37,14 +37,20 @@ void loop() {
     }
 }
 #else
-#include "ControlBoxRX.h"
 
+#include <Arduino_PortentaBreakout.h>
+#include "ControlBoxRX.h"
+breakoutPin relayPins[] = {PWM0, PWM1, PWM2, CAN1_TX, CAMERA_D0P, CAMERA_D0N, GPIO_4}; //N2,OX1,OX2,
 #define CONTROL_BOX_RECEIVE_PERIOD 200 // ms
 ControlBoxRX controlBoxRX;
+UART UART0Breakout = UART(UART0_TX, UART0_RX);
 
 void setup() {
-    Serial.begin(115200);
-    controlBoxRX.begin(&Serial); // RX=P4 in ESPWROOM32 SUBSTITUTE THE PORTENTA ONE
+    for(auto i: relayPins){
+        Breakout.pinMode(i, OUTPUT);
+        Breakout.digitalWrite(i,HIGH);
+    }
+    controlBoxRX.begin(&UART0Breakout); // RX=P4 in ESPWROOM32 SUBSTITUTE THE PORTENTA ONE
 }
 
 unsigned long lastReceiveTime = 0;
@@ -57,6 +63,25 @@ void loop() {
         Serial.print(byte(controlBoxRX.getMessage().allButtons), BIN);
         Serial.print(", ");
         Serial.println(answer);
+        if (answer == 0) {
+            Breakout.digitalWrite(relayPins[0],controlBoxRX.getMessage().ignitionButton?HIGH:LOW);
+            Breakout.digitalWrite(relayPins[1],controlBoxRX.getMessage().oxidizerValveButton?HIGH:LOW);
+            Breakout.digitalWrite(relayPins[2],controlBoxRX.getMessage().n2FeedingButton?HIGH:LOW);
+            Breakout.digitalWrite(relayPins[3],controlBoxRX.getMessage().dumpValveButton?HIGH:LOW);
+            Breakout.digitalWrite(relayPins[4],controlBoxRX.getMessage().heatingBlanket1Switch?HIGH:LOW);
+            Breakout.digitalWrite(relayPins[5],controlBoxRX.getMessage().heatingBlanket2Switch?HIGH:LOW);
+            Breakout.digitalWrite(relayPins[6],controlBoxRX.getMessage().emergencyButton?HIGH:LOW);
+        }
+        else
+        {
+            Breakout.digitalWrite(relayPins[0],HIGH);
+            Breakout.digitalWrite(relayPins[1],HIGH);
+            Breakout.digitalWrite(relayPins[2],HIGH);
+            Breakout.digitalWrite(relayPins[3],HIGH);
+            Breakout.digitalWrite(relayPins[4],HIGH);
+            Breakout.digitalWrite(relayPins[5],HIGH);
+            Breakout.digitalWrite(relayPins[6],HIGH);
+        }
     }
 }
 
