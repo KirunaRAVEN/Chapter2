885dba1144fc5ad5d1c299bb1b1da4d9b21d73af
Sal0ram <viggorocket@gmail.com>
2026-04-23 15:01:59 +0200

    Changed dump-valve from servo to solenoid (Dunkan)

--- portenta/pinMap.h
+++ portenta/pinMap.h
@@ -25,6 +25,7 @@ enum ControlBoxButton { // TODO
 #define    NITROGEN_RELAY      28
 #define    IGNITION_ARM        29
 #define    IGNITION_RELAY      47
+#define    DUMP_RELAY          11
 
 /* NORMAL OUTPUT PINS */
 #define    TEST_LED_SIGNAL     35
@@ -32,7 +33,7 @@ enum ControlBoxButton { // TODO
 #define    HIGH_SPEED_SIGNAL   32
 
 /* OTHER GPIO */
-#define    DUMP_PIN            31
+#define    DUMP_PIN            11
 #define    TEST_MODE_BUTTON    37
 #define    LOADCELL_DATA       44
 #define    LOADCELL_CLOCK      45
